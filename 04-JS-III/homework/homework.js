19f00899faa081478472734093ba84ec1c9d0081
LEXDEIT <godhood@proton.me>
2022-11-15 04:35:01 -0600

    04-JS-III/homework/homework.js

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -113,7 +113,7 @@ function numeroMasGrande(numeros) {
 
 function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
-  // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
+  // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
   var acc = 1;
   if(arguments.length === 0) {
@@ -144,16 +144,10 @@ function diaDeLaSemana(numeroDeDia) {
   /*Suponga que los días de la semana se codifican como 1 = Domingo, 2 = Lunes y así sucesivamente. 
   Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
-  Escribe tu código aquí*/   
-  var dias =["Domingo","Lunes","Martes","Miercoles","Jueves","Viernes","Sabado"];
-  
-  for (var i = 0;  i < numeroDeDia.length; i++){
-    if (numeroDeDia[i] === 1 || 7){
-      return "Es fin de Semana";
-} if (numeroDeDia[i] === 2 || 3 || 4  || 5){
-      return "Es dia Laboral";
-    }
-  }
+  Escribe tu código aquí*/
+  if (numeroDeDia === 1 || numeroDeDia === 7) {
+    return "Es fin de semana";
+  } else return "Es dia Laboral";
 } 
 
 
@@ -161,7 +155,11 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  
+  let num = n.toString()
+  if(num.charAt(0) === "9") {
+    return true
+  }
+  return false
 }
 
 
